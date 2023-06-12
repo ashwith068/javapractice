5be9aa59f48b79a29fa73451ea657e75a2b791dc
CHERUKURI ASHWITH <136350850+ashwith068@users.noreply.github.com>
2023-06-12 21:25:42 +0530

    Add files via upload
    Signed-off-by: CHERUKURI ASHWITH <136350850+ashwith068@users.noreply.github.com>

--- sul.java
+++ sul.java
@@ -3,20 +3,43 @@ import java.io.*;
 class pattern{
 public static void main(String args[])
 {
-for(int i=0;i<4;i++)
+for(int i=1;i<=4;i++)
 {
-  for(int j=0;j<5;j++)
-{
-   if(i==0 || j==0 ||i==3||j==4)
-
+ for(int j=1;j<=i;j++)
 {
    System.out.print("*"); 
 }
-else
+for(int k=1;k<=4-i+1;k++)
 {
   System.out.print(" ");
 }
-
+for(int k=1;k<=4-i+1;k++)
+{
+  System.out.print(" ");
+}
+for(int j=1;j<=i;j++)
+{
+   System.out.print("*"); 
+}
+System.out.println();
+}
+for(int i=1;i<=4;i++)
+{
+ for(int j=1;j<=4-i+1;j++)
+{
+   System.out.print("*"); 
+}
+for(int k=1;k<=i;k++)
+{
+  System.out.print(" ");
+}
+for(int k=1;k<=i;k++)
+{
+  System.out.print(" ");
+}
+for(int j=1;j<=4-i+1;j++)
+{
+   System.out.print("*"); 
 }
 System.out.println();
 }
